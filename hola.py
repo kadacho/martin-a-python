80304bf8f7a7431270248ca1d65b51d546eda029
kadacho <martinamigo35@gmail.com>
2026-04-17 18:10:07 -0400

    ejercicios for

---
+++ hola.py
@@ -0,0 +1,28 @@
+# # for nigga in range(5):
+# #     print("cfffff")
+
+# num=int(input("hoal cffff pon un número po loco: "))
+
+# for nigga in range(num):
+#     print(nigga+1, "repetición")
+# print("segunda forma")
+# for nigga in range(num):
+#     print("repetición", nigga+1)
+
+# num=int(input("Ingrese un número: "))
+
+# for primernumero in range(num):
+#     print(" ")
+#     print("Tabla del", primernumero+1)
+#     for segundonumero in range(10):
+#         print(primernumero+1,"x",segundonumero+1,"=",(primernumero+1)*(segundonumero+1))
+
+print("Hola nigger")
+
+titulo="Clima de hoy"
+diaDelMes=17
+mes=4
+tem=25.7
+llueve=True
+print(titulo, "temperatura: ", tem,diaDelMes, "/", mes)
+print(f)
---
+++ sdads.py
@@ -0,0 +1,38 @@
+# tnota=0
+
+# num=int(input("Introduzca el número de notas: "))
+# for i in range(num):
+#     nota=float(input(f"Nota {i+1}: "))
+#     tnota=tnota+nota
+
+# resultado=tnota/num
+# print("Su promedio es", round(resultado, 1))
+
+# if resultado>=4:
+#     print("Usted aprobó")
+# else:
+#     print("Usted reprobó")
+
+# num=int(input("Introduzca un número: "))
+# total=0
+
+# for i in range(num):
+#     total=total+i+1
+
+# print("La sumatoria es", total)
+
+# vocales=0
+# conso=0
+# nigga=input("Introduzca su nombre: ")
+
+# for i in nigga:
+#     print(i)
+#     if i in "aeiouAEIOU":
+#         vocales=vocales+1
+#     elif i==" ":
+#         conso+0
+#     else:
+#         conso=conso+1
+# print(f"El total de vocales es", vocales)
+# print(f"El total de consonantes es", conso)
+
